1473d060fc5a3c890210729dc4054660b9c01ff8
laptop <rahman.hasri@gmail.com>
2019-02-15 11:47:19 +0700

    upload file component

--- assets/components/main.js
+++ assets/components/main.js
@@ -1,9 +1,25 @@
-new Vue ({
+let urlServer = `http://localhost:3000`
+
+var app = new Vue ({
   el :'#app',
   data : {
-
   },
-  created : {
-  
+  methods : {
+    submitUpload(newPost) {
+      
+      axios({
+        method: 'post',
+        url: urlServer + '/upload',
+        data : newPost,
+        headers: {'Content-Type' : 'multipart/form-data'}
+      })
+        .then( response => {
+          console.log(response)
+        })
+        .catch( ({response}) => {
+          console.log(response)
+        })
+      // console.log(Object.keys(newPost))
+    }
   }
 })
