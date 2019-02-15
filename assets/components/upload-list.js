b7b62ac83b13641fcb6f51427a298a39531f2f2e
isrotrip <isrotrip93@gmail.com>
2019-02-15 16:18:06 +0700

    add-share

--- assets/components/upload-list.js
+++ assets/components/upload-list.js
@@ -13,6 +13,9 @@ Vue.component('upload-list', {
             <td> <small>Uploader</small> </td> <td><small> : </small></td> <td> <small>{{upload.uploader}}</small> </td>
           </tr>
         </table>
+        <a target="_blank" :href="'https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&sdk=joey&u=' + content.pdfUrl + '&display=popup&ref=plugin&src=share_button'">
+          Share<i class="material-icons prefix">share</i>
+        </a>
       </div>
     </div>
   `,
